acd0cd033db079ddc8ec35114b961d57a6ec937d
Galih Agus Saputra <agussaputragalih@gmail.com>
2024-08-23 11:44:52 +0700

    Onboarding widget screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:clean_ease/features/authentication/screens/onboarding.dart';
+import 'package:clean_ease/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:clean_ease/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
