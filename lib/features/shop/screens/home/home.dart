2cf3cd221c94970860ee466aab1f1c7041f06a84
Galih Agus Saputra <agussaputragalih@gmail.com>
2024-08-27 03:53:16 +0700

    Promo Slider Controller Images

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,11 +1,9 @@
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:clean_ease/common/widgets/custom_shapes/containers/circular_container.dart';
 import 'package:clean_ease/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:clean_ease/common/widgets/custom_shapes/containers/search_container.dart';
-import 'package:clean_ease/common/widgets/images/app_rounded_image.dart';
 import 'package:clean_ease/common/widgets/texts/section_heading.dart';
 import 'package:clean_ease/features/shop/screens/home/widget/home_appsbar.dart';
 import 'package:clean_ease/features/shop/screens/home/widget/home_categories.dart';
+import 'package:clean_ease/features/shop/screens/home/widget/promo_slider.dart';
 import 'package:clean_ease/utils/constants/image_strings.dart';
 import 'package:clean_ease/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
@@ -15,7 +13,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -54,31 +52,12 @@ class HomeScreen extends StatelessWidget {
 
             // Body
             Padding(
-              padding: const EdgeInsets.all(AppSize.defaultSpace),
-              child: Column(
-                children: [
-                  CarouselSlider(
-                    options: CarouselOptions(
-                      viewportFraction: 1,
-                    ),
-                    items: [
-                      AppRoundedImage(imageUrl: AppImages.promoBanner1),
-                      AppRoundedImage(imageUrl: AppImages.promoBanner2),
-                      AppRoundedImage(imageUrl: AppImages.promoBanner3),
-                    ],
-                  ),
-                  const SizedBox(height: AppSize.spaceBtwItems),
-                  Row(
-                    children: [
-                      for (int i = 0; i < 3; i++)
-                        const AppCircularContainer(
-                          width: 20,
-                          height: 4,
-                          margin: EdgeInsets.only(right: 10),
-                          backgroundColor: Colors.green,
-                        ),
-                    ],
-                  )
+              padding: EdgeInsets.all(AppSize.defaultSpace),
+              child: AppPromoSlider(
+                banners: [
+                  AppImages.promoBanner1,
+                  AppImages.promoBanner2,
+                  AppImages.promoBanner3
                 ],
               ),
             ),
